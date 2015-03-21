c438cf9b3b56f8babae48e19edaa87feab05d7fd
David Drayton <draytond@gmail.com>
2015-03-21 17:54:45 -0400

    thing further removed, trying to start adding authenication

--- server/config/seed.js
+++ server/config/seed.js
@@ -5,11 +5,11 @@
 
 'use strict';
 
-var Thing = require('../api/thing/thing.model');
+var Task = require('../api/task/task.model');
 
 
-Thing.find({}).remove(function() {
-  Thing.create({
+Task.find({}).remove(function() {
+  Task.create({
     name : 'Development Tools',
     info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
   }, {
